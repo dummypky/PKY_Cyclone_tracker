aa1f1f6120c0745cab637d360a7a67de9800d5b9
Prashanna <erpky.nitr@gmail.com>
2024-10-24 22:18:18 +0530

    mobile version

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.write("Real-time tracking of cyclones around India and Southeast Asia, with N
 
 # Embed Windy.com iframe for real-time weather tracking
 windy_embed_code = """
-<iframe width="650" height="450" src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface&lat=18.521&lon=84.902&detailLat=22.243&detailLon=84.909&detail=true&pressure=true&message=true" frameborder="0"></iframe>
+<iframe width="650" height="450" src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=5&overlay=wind&product=ecmwf&level=surface&lat=16.51&lon=85.825&detailLat=20.271&detailLon=85.833&detail=true" frameborder="0"></iframe>
 """
 
 # Display the iframe with Windy.com cyclone tracking
